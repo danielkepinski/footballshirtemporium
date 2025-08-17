f4e22490f8883aa0c69fa27c1e8301b1f26b3611
danielkepinski <l397waws@students.codeinstitute.net>
2025-08-17 21:43:23 +0100

    created first product

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Category model.
+    """
+
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Product model.
+    """
+
+    list_display = [
+        "name",
+        "slug",
+        "price",
+        "available",
+        "created",
+        "updated",
+    ]
+    list_filter = ["available", "created", "updated"]
+    list_editable = ["price", "available"]
+    prepopulated_fields = {"slug": ("name",)}
