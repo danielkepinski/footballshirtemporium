3772b81ed844308413710680a592d9b9b28f483f
danielkepinski <l397waws@students.codeinstitute.net>
2025-09-07 21:36:21 +0100

    fixed some pathways and created urls.py in shop to seperate the django and the application

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -1,27 +1,17 @@
 """
 URL configuration for myshop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
-
-app_name = "shop"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.product_list, name="product_list"),
-    path("<slug:category_slug>/", views.product_list, name="product_list_by_category"),
-    path("products/<int:id>/<slug:slug>/", views.product_detail, name="product_detail"),
+    path("", include(("shop.urls", "shop"), namespace="shop")),  # delegate to app
 ]
 
-# Serve static files when DEBUG is False (for local testing only).
+# Serve static + media in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
