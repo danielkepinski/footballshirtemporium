f1e8cd525fe2b859efd0a64ae58f7505f9bdb668
danielkepinski <l397waws@students.codeinstitute.net>
2025-09-10 13:23:00 +0100

    did some housekeeping with file paths

--- myshop/settings.py
+++ myshop/settings.py
@@ -23,8 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop.apps.ShopConfig",
-    "cart.apps.CartConfig",  #added shopping cart
-    "shop.apps.ShopConfig",  
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
