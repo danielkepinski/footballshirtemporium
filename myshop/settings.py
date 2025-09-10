d495095e52f495fdda222fa78eb6e9ab1ae31566
danielkepinski <l397waws@students.codeinstitute.net>
2025-09-10 13:00:04 +0100

    added cart.py and added template

--- myshop/settings.py
+++ myshop/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",  #added shopping cart
+    "shop.apps.ShopConfig",  
 ]
 
 MIDDLEWARE = [
