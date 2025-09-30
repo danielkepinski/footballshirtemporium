3027c1deaf8285fea3604666b3621266595d12db
danielkepinski <l397waws@students.codeinstitute.net>
2025-09-30 10:20:17 +0100

    added urls to myshop/urls to include being able to log in

--- myshop/urls.py
+++ myshop/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("cart/", include(("cart.urls", "cart"), namespace="cart")),
     path("orders/", include(("orders.urls", "orders"), namespace="orders")),
     path("payment/", include(("payment.urls", "payment"), namespace="payment")),  
+    path("accounts/", include(("accounts.urls", "account"), namespace="account")),
+    path("accounts/", include("django.contrib.auth.urls")), #login/password reset!
     path("", include(("shop.urls", "shop"), namespace="shop")),
 ]
 
