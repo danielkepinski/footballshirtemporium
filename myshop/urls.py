dc1356f3f36c0d18946e013a7484611756dfd18c
danielkepinski <l397waws@students.codeinstitute.net>
2025-10-01 21:05:16 +0100

    chages didnt save so reloaded my code into addresses/views.py

--- myshop/urls.py
+++ myshop/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("payment/", include(("payment.urls", "payment"), namespace="payment")),  
     path("accounts/", include(("accounts.urls", "account"), namespace="account")),
     path("accounts/", include("django.contrib.auth.urls")), #login/password reset!
+    path("addresses/", include(("addresses.urls", "addresses"), namespace="addresses")), 
     path("", include(("shop.urls", "shop"), namespace="shop")),
 ]
 
