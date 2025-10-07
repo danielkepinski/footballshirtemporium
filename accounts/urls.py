1c0d3a5047d4183b4d79d71aae38f7ae706141f0
danielkepinski <l397waws@students.codeinstitute.net>
2025-10-07 20:43:48 +0100

    fixing pathing error for logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("orders/<int:pk>/", views.order_detail, name="order_detail"),
     path("profile/", views.profile_edit, name="profile"),            
     path("delete/", views.delete_account, name="delete_account"),    
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
