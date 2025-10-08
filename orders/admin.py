779371a6b2cb9f10087a3cfed348930869e8c6e9
danielkepinski <l397waws@students.codeinstitute.net>
2025-10-08 13:03:13 +0100

    updated a missing piece of code that was making push to heroku fail, this was from my previous update of order payment in admin.py in orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,92 +1,83 @@
+# orders/admin.py
 import csv
 import datetime
 
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import reverse
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html  
 
 from .models import Order, OrderItem
 
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
-    content_disposition = (
-        f'attachment; filename={opts.verbose_name}.csv'
-    )
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = content_disposition
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = f'attachment; filename="{opts.verbose_name}.csv"'
     writer = csv.writer(response)
+
     fields = [
-        field
-        for field in opts.get_fields()
-        if not field.many_to_many and not field.one_to_many
+        f for f in opts.get_fields()
+        if not f.many_to_many and not f.one_to_many
     ]
-    # Write a first row with header information
-    writer.writerow([field.verbose_name for field in fields])
-    # Write data rows
+    writer.writerow([f.verbose_name for f in fields])
+
     for obj in queryset:
-        data_row = []
-        for field in fields:
-            value = getattr(obj, field.name)
+        row = []
+        for f in fields:
+            value = getattr(obj, f.name)
             if isinstance(value, datetime.datetime):
-                value = value.strftime('%d/%m/%Y')
-            data_row.append(value)
-        writer.writerow(data_row)
+                value = value.strftime("%d/%m/%Y")
+            row.append(value)
+        writer.writerow(row)
     return response
 
-
-export_to_csv.short_description = 'Export to CSV'
+export_to_csv.short_description = "Export to CSV"
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    raw_id_fields = ['product']
-
-
-def order_payment(self, obj):
-        url = obj.get_stripe_url()
-        if not url:
-            return "-"
-        return format_html('<a href="{}" target="_blank" rel="noopener">Stripe</a>', url)
-    order_payment.short_description = "Stripe"
-    
-
-
-order_payment.short_description = 'Stripe payment'
-
-
-def order_detail(obj):
-    url = reverse('orders:admin_order_detail', args=[obj.id])
-    return mark_safe(f'<a href="{url}">View</a>')
-
-
-def order_pdf(obj):
-    url = reverse('orders:admin_order_pdf', args=[obj.id])
-    return mark_safe(f'<a href="{url}">PDF</a>')
-
-
-order_pdf.short_description = 'Invoice'
+    raw_id_fields = ["product"]
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
-        'id',
-        'first_name',
-        'last_name',
-        'email',
-        'address',
-        'postal_code',
-        'city',
-        'paid',
-        order_payment,
-        'created',
-        'updated',
-        order_detail,
-        order_pdf,
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "address",
+        "postal_code",
+        "city",
+        "paid",
+        "order_payment",   
+        "created",
+        "updated",
+        "order_detail",    
+        "order_pdf",       
     ]
-    list_filter = ['paid', 'created', 'updated']
+    list_filter = ["paid", "created", "updated"]
     search_fields = ["first_name", "last_name", "email", "id"]
     inlines = [OrderItemInline]
     actions = [export_to_csv]
+
+    # admin column: link to Stripe
+    def order_payment(self, obj):
+        url = obj.get_stripe_url()
+        if not url:
+            return "-"
+        return format_html('<a href="{}" target="_blank" rel="noopener">Stripe</a>', url)
+    order_payment.short_description = "Stripe"
+
+    # admin column: link to detail view 
+    def order_detail(self, obj):
+        url = reverse("orders:admin_order_detail", args=[obj.id])
+        return format_html('<a href="{}">View</a>', url)
+    order_detail.short_description = "Detail"
+
+    # admin column: link to PDF 
+    def order_pdf(self, obj):
+        url = reverse("orders:admin_order_pdf", args=[obj.id])
+        return format_html('<a href="{}">PDF</a>', url)
+    order_pdf.short_description = "Invoice"
