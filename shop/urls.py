cb1a35a2fa8daf068410589349dea268f60d071e
danielkepinski <l397waws@students.codeinstitute.net>
2025-10-20 18:30:08 +0100

    added contact to urls

--- shop/urls.py
+++ shop/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("<slug:category_slug>/", views.product_list, name="product_list_by_category"),
     path("", views.product_list, name="product_list"),
     path("team/<slug:team_slug>/", views.product_list, name="product_list_by_team"),
+    path("contact/", views.contact, name="contact"), 
 ]
