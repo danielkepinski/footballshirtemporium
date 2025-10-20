4ffe270135f01947bdfb515460319c8bc6d1d16b
danielkepinski <l397waws@students.codeinstitute.net>
2025-10-20 18:11:56 +0100

    added url to account/urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("orders/<int:pk>/", views.order_detail, name="order_detail"),
     path("profile/", views.profile_edit, name="profile"),            
-    path("delete/", views.delete_account, name="delete_account"),    
+    path("delete/", views.delete_account, name="delete_account"),   
+    path('account/', views.account, name='account'), 
 ]
