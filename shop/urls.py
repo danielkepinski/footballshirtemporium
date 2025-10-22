ecc0b285efec9c7756620654924ec3017c0ec690
danielkepinski <l397waws@students.codeinstitute.net>
2025-10-22 13:33:29 +0100

    i did not press save on shop/urls so it didnt push change, search shold now work

--- shop/urls.py
+++ shop/urls.py
@@ -6,9 +6,9 @@ app_name = "shop"
 
 urlpatterns = [
     path("product/<int:id>/<slug:slug>/", views.product_detail, name="product_detail"),
-    path("<slug:category_slug>/", views.product_list, name="product_list_by_category"),
-    path("", views.product_list, name="product_list"),
-    path("team/<slug:team_slug>/", views.product_list, name="product_list_by_team"),
-    path("contact/", views.contact, name="contact"), 
+    path("team/<slug:team_slug>/", views.product_list, name="product_list_by_team"),  # 
+    path("contact/", views.contact, name="contact"),
     path("search/", views.search, name="search"),
+    path("", views.product_list, name="product_list"),
+    path("<slug:category_slug>/", views.product_list, name="product_list_by_category"),  
 ]
