4bf63b806a4e302703b67afba87bf3a3e8f13593
danielkepinski <l397waws@students.codeinstitute.net>
2026-01-07 12:21:40 +0000

    added frame for am i responsive

--- myshop/settings.py
+++ myshop/settings.py
@@ -210,3 +210,5 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
+# --- am i responsive ---
+X_FRAME_OPTIONS = "ALLOWALL"
